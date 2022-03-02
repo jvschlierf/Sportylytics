3128519571e1c32b636f6b70a694e3cddaf7aa84
Alberto Allegri <alberto1998@hotmail.it>
2022-03-02 22:42:37 +0100

    Quick change

--- Datasets/Dataset_Scraper.R
+++ Datasets/Dataset_Scraper.R
@@ -2,17 +2,20 @@
 rm(list=ls()) 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#Library for scraping data from FBref.com
 #devtools::install_github("JaseZiv/worldfootballR")
-
 library(worldfootballR)
+
+#Saving as a dataframe the data regarding FBref & Transfermarkt links for every player
 mapped_players <- player_dictionary_mapping()
 
-players = mapped_players[1:50,"PlayerFBref"]
+#Taking only first 100 players
+players = mapped_players[1:200,"PlayerFBref"]
 numb = dim(players)[1]
 
+#Initializing our dataframes
 df_players = data.frame()
 df_keepers = data.frame()
-df_mark_value = data.frame()
 
 for (i in (1:numb)){
   tryCatch({
@@ -41,31 +44,25 @@ for (i in (1:numb)){
     rownames(players_data) <- unlist(c(mapped_players[i,"PlayerFBref"]))
     
     #Add the TMK Value as a new column and reduce the dataset
-    new_player_TRM$player_valuation = as.integer(unlist(c(new_player_TRM[,"player_valuation"])))
-    new_player_TRM = new_player_TRM[,c("player_name","player_valuation")]
+    players_data$Player_valuation = as.integer(unlist(c(new_player_TRM[,"player_valuation"])))
     
     #Append the dataframes (firstly the one for VALUES, then for PLAYERS, basing on position)
-    df_mark_value = rbind(df_mark_value, new_player_TRM)
-    if (isTRUE(unlist(c(new_player_FB[2,"Versus"]))=="Goalkeepers") == TRUE){
-      df_keepers = rbind(df_keepers, players_data)
-    } else {
-      df_players = rbind(df_players, players_data)
-    }
+    if (isTRUE(dim(players_data)[2] >= 2) == TRUE){
+      if (isTRUE(unlist(c(new_player_FB[2,"Versus"]))=="Goalkeepers") == TRUE){
+        df_keepers = rbind(df_keepers, players_data)
+        } else {
+        df_players = rbind(df_players, players_data)
+        }
+    } else {next}
   }, error = function(e){})
 }
 
-#Reindexing to merge the datasets
-rownames(df_mark_value) = df_mark_value$player_name
+#Making Index the first column (Player Name)
+df_players = cbind(Player_Names = rownames(df_players), df_players)
+rownames(df_players) = 1:nrow(df_players)
 
-#Finalizing players by merging the two datasets on players names and deleting extra row
-df_players = merge(df_players, df_mark_value, by.x = 0, by.y = 0)
-df_players = df_players[,c(which(colnames(df_players)=="player_name"),which(colnames(df_players)!="player_name"))]
-df_players = subset(df_players, select = -c(Row.names))
-
-#Finalizing keepers by merging the two datasets on players names and deleting extra row
-df_keepers = merge(df_keepers, df_mark_value, by.x = 0, by.y = 0)
-df_keepers = df_keepers[,c(which(colnames(df_keepers)=="player_name"),which(colnames(df_keepers)!="player_name"))]
-df_keepers = subset(df_keepers, select = -c(Row.names))
+df_keepers = cbind(Keeper_Names = rownames(df_keepers), df_keepers)
+rownames(df_keepers) = 1:nrow(df_keepers)
 
 #Saving the two datasets
 write.csv(df_players,'df_players.csv')
