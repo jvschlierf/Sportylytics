498ccd752809e881e1815c30a3b4e56daf475ae0
jvschlierf <67417254+jvschlierf@users.noreply.github.com>
2022-03-31 23:15:28 +0200

    Added File for KNN Training

---
+++ KNN/KNN_train_01.R
@@ -0,0 +1,16 @@
+# 20630 - Introduction to Sports Analytics
+# Group 2 Project 3 - Supervised Machine Learning Model
+# Model 2: K-Nearest Neighbor
+
+# clear environment & set working directory
+rm(list=ls()) 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Package import
+my_packages <- c("quantmod","PerformanceAnalytics","ellipse","reshape2","ggplot2", "rvest", "stringr",
+                 "dygraphs", "dplyr","forecast", "aod","readr","rvest","lubridate", "xml2", "caret")
+lapply(my_packages, require, character.only = TRUE)
+
+#Load Data (both train and test)
+train_basket <- read.csv('../Dataset/data_Bplayers_2000_TRAIN.csv')
+test_basket <- read.csv('../Dataset/data_Bplayers_2000_TEST.csv')
