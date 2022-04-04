3643663d04ade3cc96199f346a3b35937e69743d
Alberto Allegri <alberto1998@hotmail.it>
2022-04-04 12:46:34 +0200

    hyper_grid update

--- Gradient Boosting/Gradient_Boosting_train_01.R
+++ Gradient Boosting/Gradient_Boosting_train_01.R
@@ -7,17 +7,19 @@ rm(list=ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Package import
-listofpackages <- c("quantmod","PerformanceAnalytics","ellipse","reshape2","ggplot2", "rvest", "stringr",
-                 "dygraphs", "dplyr","forecast", "aod","readr","rvest","lubridate", "xml2", "caret", 
-                 "gbm", "fastDummies")
+listofpackages <- c("quantmod","PerformanceAnalytics","ellipse","reshape2","ggplot2", "rvest",
+                    "stringr", "dygraphs", "dplyr","forecast", "aod","readr","rvest","lubridate", 
+                    "xml2", "caret", "gbm", "fastDummies", "pdp")
 
-#Uploading libraries
+# Uploading libraries
 for (j in listofpackages){
   if(sum(installed.packages()[, 1] == j) == 0) {
     install.packages(j)
   }
   library(j, character.only = T)
 }
+
+### 1. PREPROCESSING ###
 
 #Load Data (both train and test)
 train_basket <- read.csv('../Dataset/data_Bplayers_2000_TRAIN.csv')
@@ -48,24 +50,90 @@ test_basket = pre_treat(test_basket)
 test_basket_x = subset(test_basket, select = -Salary_Cap_Perc) # feature and target array
 test_basket_y = test_basket[, "Salary_Cap_Perc"]
 
-#Now we can tune Gradient Boosting
-model_gbm = gbm(train_basket$Salary_Cap_Perc ~.,
-                data = train_basket,
-                distribution = "gaussian",
-                cv.folds = 5,
-                n.minobsinnode = 10,
-                shrinkage = 0.001,
-                n.trees = 15000)
+### 2. TUNING OF GRADIENT BOOSTING MODEL ###
 
-#Now we test the model
-pred_basket_y = predict.gbm(model_gbm, test_basket_x)
+hyper_grid <- expand.grid(
+  shrinkage = c(.01, .1, .3),
+  interaction.depth = c(1, 3, 5),
+  n.minobsinnode = c(5, 10, 15),
+  bag.fraction = c(.65, .8, 1), 
+  optimal_trees = 0,               # a place to dump results
+  min_RMSE = 0                     # a place to dump results
+)
 
-#Analysis of results
+# total number of combinations
+nrow(hyper_grid)
+
+# randomize data
+random_index <- sample(1:nrow(train_basket), nrow(train_basket))
+random_basket_train <- train_basket[random_index, ]
+
+# grid search 
+for(i in 1:nrow(hyper_grid)) {
+  
+  print(i)
+  
+  # reproducibility
+  set.seed(123)
+  
+  # train model
+  gbm_tune <- gbm(
+    formula = Salary_Cap_Perc ~ .,
+    distribution = "gaussian",
+    cv.folds = 5,
+    data = random_basket_train,
+    n.trees = 5000,
+    interaction.depth = hyper_grid$interaction.depth[i],
+    shrinkage = hyper_grid$shrinkage[i],
+    n.minobsinnode = hyper_grid$n.minobsinnode[i],
+    bag.fraction = hyper_grid$bag.fraction[i],
+    train.fraction = .8,
+    n.cores = NULL, # will use all cores by default
+    verbose = FALSE
+  )
+  
+  # add min training error and trees to grid
+  hyper_grid$optimal_trees[i] <- which.min(gbm_tune$valid.error)
+  hyper_grid$min_RMSE[i] <- sqrt(min(gbm_tune$valid.error))
+}
+
+hyper_grid %>% 
+  dplyr::arrange(min_RMSE) %>%
+  head(10)
+
+### 3. TRAINING ON THE BEST MODEL ###
+
+set.seed(123)
+
+# train GBM model
+gbm_final <- gbm(
+  formula = Salary_Cap_Perc ~ .,
+  distribution = "gaussian",
+  data = basket_train,
+  n.trees = 10000,
+  interaction.depth = 5,
+  shrinkage = 0.1,
+  n.minobsinnode = 5,
+  bag.fraction = .65, 
+  train.fraction = 1,
+  n.cores = NULL, # will use all cores by default
+  verbose = FALSE
+) 
+
+### 4. PREDICTION ###
+
+pred_basket_y = predict.gbm(gbm_final, test_basket_x)
+
+### 5. ANALYSIS OF RESULTS ###
 RMSE = sqrt(min(model_gbm$cv.error))
 cat('The root mean square error of the test data is ', round(RMSE,3),'\n')
+#RMSE is 
 
 rsq <- (cor(pred_basket_y, test_basket$Salary_Cap_Perc))^2
 cat('The R-square of the test data is ', round(rsq,3), '\n')
+#R-square is 
+
+gbm.perf(model_gbm, method = "cv")
 
 # visualize the model, actual and predicted data
 x_ax = 1:length(pred_basket_y)
@@ -73,9 +141,9 @@ plot(x_ax, test_basket_y, col="blue", pch=20, cex=.9)
 lines(x_ax, pred_basket_y, col="red", pch=20, cex=.9) 
 
 #Relative influence
-summary(
-  model_gbm, 
-  cBars = 10,
-  method = relative.influence, # also can use permutation.test.gbm
-  las = 2
-  )
+summary(model_gbm, cBars = 10, method = relative.influence, las = 2)
+
+#Partial dependence
+model_gbm %>%
+     partial(pred.var = "ws", n.trees = model_gbm$n.trees, grid.resolution = 100) %>%
+     autoplot(rug = TRUE, train = train_basket)
