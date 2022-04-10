cbbebce8f5c28e35254a445a862eb8f722c1d437
Federico Leonardi <federico.leonardi@studbocconi.it>
2022-04-10 22:58:35 +0200

    added contracts preprocessing

---
+++ Dataset/spotrac_preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+### WORK IN PROGRESS:
+
+df = pd.read_csv('spotrac_contracts.csv')
+
+counts = pd.read_csv('value_counts.csv')
+for value in counts['Unnamed: 0']:
+    print(value)
+
