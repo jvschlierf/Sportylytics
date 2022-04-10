ac4172e764f401a5a10c46b8eba43ce564eab4c9
Federico Leonardi <federico.leonardi@studbocconi.it>
2022-04-10 22:12:13 +0200

    commented scraping from spotrac script

--- Dataset/spotrac_scraping.py
+++ Dataset/spotrac_scraping.py
@@ -69,23 +69,41 @@ player_contracts = pd.DataFrame(columns=['player_name', 'season', 'starting_year
 
 path = "webpages" # Path where we dump the txts
 
-# Iterate over each player (page)
+# Iterate over each player (webpage)
 for i, row in page_index.iterrows():
     player_name = row['player_name']
     page_name = player_name.replace(' ', '_')
     path = "webpages"
     page = open(os.path.join(path,page_name) + ".txt", 'rb')
     soup = BeautifulSoup(page, "html.parser")
-    try:
+
+    # Each individual player Spotrac webpage is organized in this way:
+    ## - Current contract, broke down year-by-year          --> Already in the appropriate format for us 
+    ## - (Optional) Table with an already signed extension  --> We can to skip this, not relevant for us
+    ## - All the previous contracts, just summarized        --> We have to unpack them, and broke them down year by year 
+
+    try: # We need to handle a few exceptions, pages "missing" from the website but present in our index
+
+        # Get the years of the current contract
         current_years = [x.get_text() for x in soup.find_all("td", class_="salaryYear center") if len(x.get_text()) > 4]
-        current_signed =  [x.get_text()[14:] for x in soup.find("table", class_="salaryTable salaryInfo hidden-xs").find_all("td", class_='contract-item') if x.get_text().startswith(" Signed Using:")][0]
+        # Get the current contract type
+        current_signed = [x.get_text()[14:] for x in 
+                           soup.find("table", class_="salaryTable salaryInfo hidden-xs").find_all("td", class_='contract-item') 
+                           if x.get_text().startswith(" Signed Using:")][0]
+        # Get the time spans of the previous contracts
         previous_years = [x.get_text()[:9] for x in soup.find_all("span", class_="contract-type-years")]
-        previous_signed = [x.get_text()[14:] for y in soup.find_all("table", class_="salaryTable salaryInfo hidden-xs")[-len(previous_years):] for x in y.find_all("td", class_='contract-item') if x.get_text().startswith(" Signed Using:")]
+        # Get the previous contract types
+        previous_signed = [x.get_text()[14:] for y in 
+                           soup.find_all("table", class_="salaryTable salaryInfo hidden-xs")[-len(previous_years):] 
+                           for x in y.find_all("td", class_='contract-item') if x.get_text().startswith(" Signed Using:")]
     except AttributeError:
-        print(player_name)
+        print(player_name) # Print out the exceptions we handled
         continue
+
+    # Create a temporary empty dataframe to store all the contracts of the player we are scraping
     tmp = pd.DataFrame(columns=['player_name', 'season', 'starting_year', 'ending_year', 'signed_using'])
 
+    # Iterate over the current contract years
     for year in current_years:
         ending = int(year[-2:])
         if ending > 40:
@@ -93,43 +111,54 @@ for i, row in page_index.iterrows():
         else:
             ending_year = 2000 + ending
         starting_year = int(year[:4])
-        d = {'player_name' : player_name,
-            'season' : year,
-            'starting_year' : starting_year,
-            'ending_year' : ending_year,
-            'signed_using': current_signed}
-        tmp = tmp.append(d, ignore_index=True)
+        # Store in a dictionary the information we are looking for
+        d = {'player_name' : player_name,     # Name of the player
+            'season' : year,                  # Season, in format yyyy-yy, to match the main dataset
+            'starting_year' : starting_year,  # Starting year of the season
+            'ending_year' : ending_year,      # Ending year of the season
+            'signed_using': current_signed}   # Contract type 
+        tmp = tmp.append(d, ignore_index=True) # Append to the dataframe
 
+    # Iterate over previous contracts
     for year, signed in zip(previous_years, previous_signed):
+        # Certain players have typos/missing information (i.e. season 0000-00), we need to handle those exceptions
         try:
-            starting = int(year[:4])
-            ending = int(year[-4:])
+            starting = int(year[:4]) # Starting year of the contract
+            ending = int(year[-4:])  # Ending year of the contract
+            # Iterate over the time span of the contract to unpack it year-by-year
             for season_year in range(starting, ending+1):
-                season = str(season_year) + '-' + str(season_year+1)[-2:]
+                season = str(season_year) + '-' + str(season_year+1)[-2:] # Season, in format yyyy-yy
+                # Again, we store the information in a dictionary
                 d = {'player_name' : player_name,
                     'season' : season,
                     'starting_year' : season_year,
                     'ending_year' : season_year+1,
                     'signed_using': signed}
-                tmp = tmp.append(d, ignore_index=True)
+                tmp = tmp.append(d, ignore_index=True) # Append to the dataframe
         except ValueError:
-            print(player_name, year, signed)
+            print(player_name, year, signed) # Print out the exceptions we handled
             continue
 
-    tmp = tmp.sort_values('ending_year', ascending=False).reset_index(drop=True)
+    tmp = tmp.sort_values('ending_year', ascending=False).reset_index(drop=True) # Order the dataframe by year, descending
+
+    # We need to avoid duplicates, in particular:
+    ## - Certain players have signed extension to contracts running out during the same season
+    ## - Certain players have changed team during the season (same contract, different team)
     tmp = tmp.drop_duplicates(subset=['season', 'starting_year', 'ending_year'], keep='first').reset_index(drop=True)
-    player_contracts = player_contracts.append(tmp, ignore_index=True)
-    page.close()    
+    player_contracts = player_contracts.append(tmp, ignore_index=True) # Append to the final dataframe
+    page.close() 
 
 # Save to csv
 player_contracts.to_csv('spotrac_contracts.csv', index=False)
 
-# Let's cleanup from all the txt files
+# Let's cleanup all the txt files
 for file in os.listdir(path):
     if file == 'all_players.txt':
         continue
     else:
         os.remove(os.path.join(path, file))
+
+# Let's see all the exceptions we have handled, luckily there are not major players and/or contracts
 
 """
 Exceptions Handled:
