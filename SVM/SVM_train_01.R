fe1c166aa579ccf125050840bc7973b9c0b1cea5
Federico Leonardi <federico.leonardi@studbocconi.it>
2022-04-13 02:35:10 +0200

    updated SVM

--- SVM/SVM_train_01.R
+++ SVM/SVM_train_01.R
@@ -24,8 +24,8 @@ for (j in listofpackages){
 ### 1. PREPROCESSING ###
 
 #Load Data (both train and test)
-train_basket <- read.csv('../Dataset/data_Bplayers_2000_TRAIN.csv')
-test_basket <- read.csv('../Dataset/data_Bplayers_2000_TEST.csv')
+train_basket <- read.csv('../Dataset/Final Datasets/Final_data_Bplayers_2000_TRAIN.csv')
+test_basket <- read.csv('../Dataset/Final Datasets/Final_data_Bplayers_2000_TEST.csv')
 
 #Creating and applying a function for common pre-treatment of train and test
 pre_treat <- function(dataset){
@@ -36,7 +36,7 @@ pre_treat <- function(dataset){
   dataset <- dummy_cols(dataset, select_columns = 'pos')
   
   #Let's drop columns we won't use
-  drops <- c("season","Player",'tm','lg','Salary_Cap','Salary', 'pos')
+  drops <- c("season","Player",'tm','lg','Salary_Cap','Salary', 'pos', 'Image_Link')
   dataset = dataset[ , !(names(dataset) %in% drops)]
   
   #We replace NA with 0
@@ -68,16 +68,24 @@ svm_radial <- train(Salary_Cap_Perc ~ ., data = train_basket, method = 'svmRadia
                     preProcess = c("center", "scale"), trCtrl = trctrl)
 
 print(svm_radial)
+# RMSE        Rsquared   MAE       
+# 0.03935554  0.7041773  0.02696496
+
 
 svm_linear <- train(Salary_Cap_Perc ~ ., data = train_basket, method = 'svmLinear',
                     preProcess = c("center", "scale"), trCtrl = trctrl)
 
 print(svm_linear)
+# RMSE        Rsquared   MAE       
+# 0.04505973  0.6282156  0.03143813
+
 
 svm_poly <- train(Salary_Cap_Perc ~ ., data = train_basket, method = 'svmPoly', 
                   preProcess = c("center", "scale"), trCtrl = trctrl)
 
 print(svm_poly)
+# degree  scale  C     RMSE        Rsquared    MAE       
+# 2       0.001  0.25  0.04467952  0.64633559  0.03117615
 
 #Predict using different kernels
 pred_radial = predict(svm_radial, newdata=test_basket_x)
@@ -90,12 +98,21 @@ RMSE_linear = sqrt(mean((test_basket_y - pred_linear)^2))
 RMSE_poly = sqrt(mean((test_basket_y - pred_poly)^2))
 
 print(c(RMSE_radial, RMSE_linear, RMSE_poly))
+# 0.03712062 0.04282188 0.03664644
 
-cor(test_basket_y, pred_radial)^2
-cor(test_basket_y, pred_linear)^2
-cor(test_basket_y, pred_poly)^2
+cor(test_basket_y, pred_radial)^2  # 0.7266582
+cor(test_basket_y, pred_linear)^2  # 0.6400517
+cor(test_basket_y, pred_poly)^2    # 0.734441
 
 ### We proceed with the best performing, the radial kernel
+
+#Save model
+saveRDS(svm_radial, file = "svm_radial.Rds")
+saveRDS(svm_poly, file = "svm_poly.Rds")
+
+#Load model
+# svm_radial <- readRDS(file = "svm_radial.Rds")
+# svm_poly <- readRDS(file = "svm_poly.Rds")
 
 
 
@@ -103,24 +120,29 @@ cor(test_basket_y, pred_poly)^2
 
 tuneGrid <- expand.grid(C = c(0.25, 0.5, 0.75, 1, 1.25, 1.5), sigma = c(0.05, 0.01, 0.015))
 
-svm_tuned <- train(Salary_Cap_Perc ~ ., data = train_basket, method = 'svmRadial', 
+svm_radial_tuned <- train(Salary_Cap_Perc ~ ., data = train_basket, method = 'svmRadial', 
                     preProcess = c("center", "scale"), trCtrl = trctrl, 
                     tuneGrid = tuneGrid)
-print(svm_tuned)
+
+print(svm_radial_tuned)
+# C     sigma  RMSE        Rsquared   MAE       
+# 1.50  0.010  0.03879415  0.7183434  0.02665650
 
 #Prediction
 pred_tuned = predict(svm_tuned, newdata=test_basket_x)
 RMSE_tuned = sqrt(mean((test_basket_y - pred_tuned)^2))
-print(c(RMSE_radial, RMSE_tuned))
-cor(test_basket_y, pred_tuned)^2
+print(c(RMSE_radial, RMSE_tuned))  # 0.03712062 0.03619767
+cor(test_basket_y, pred_tuned)^2   # 0.7398564
 
 #Save model
-saveRDS(svm_tuned, file = "svm_tuned.Rds")
+saveRDS(svm_radial_tuned, file = "svm_radial_tuned.Rds")
 
 #Load model
-# svm_tuned <- readRDS(file = "svm_tuned.Rds")
-### RMSE_tuned = 0.04249109
+# svm_radial_tuned <- readRDS(file = "svm_radial_tuned.Rds")
+### RMSE_radial_tuned = 0.03619767
 
+
+######### SVM POLY TUNED #########
 
 
 ### 4. FEATURES SELECTION ###
@@ -142,16 +164,6 @@ svm_features_cv <- readRDS(file = "svm_features_cv.Rds")
 print(svm_features_cv)
 
 
-#Recursive feature elimination (using bootstrap)
-svm_features_bs <- rfe(train_basket_x, train_basket_y, sizes = c(5, 10, 20, 30, 40, 52),
-                    rfeControl = rfeControl(functions = caretFuncs, number = 200), method = "svmRadial")
-
-#Save features
-saveRDS(svm_features_bs, file = "svm_features_bs.Rds")
-
-#Load features
-# svm_features_bs <- readRDS(file = "svm_features_bs.Rds")
-print(svm_features_bs)
-
 ### No significant improvement, we keep the full model
-### Best RMSE = 0.04249109
+### Best RMSE = 
+
